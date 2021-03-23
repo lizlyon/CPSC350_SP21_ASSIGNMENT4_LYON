2bcb45f585b1a4401d0c1f8f790a62d66aed85aa
Liz Lyon <elyon@chapman.edu>
2021-03-22 20:41:37 -0700

    updated

--- RPNCalc.h
+++ RPNCalc.h
@@ -1,12 +1,14 @@
 #ifndef RPN_CALC_H
 #define RPN_CALC_H
 #include <string>
-#include "Stack.h"
+#include "GenStack.h"
 
 using namespace std;
 
 class RPNCalc{
 public:
+	RPNCalc();
+	~RPNCalc();
 	void calculate(string& operand);
 };
 #endif
